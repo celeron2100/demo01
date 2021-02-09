cc6f0139f292fead2f63563ff254fc6e1dc0919b
celeron2100 <celeron2100@gmail.com>
2021-02-10 00:46:08 +0800

    commit

---
+++ src/main/java/demo01/HomeController.java
@@ -0,0 +1,17 @@
+package demo01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    public HomeController() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
